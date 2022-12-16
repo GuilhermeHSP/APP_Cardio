2ebc4894920f3779c1dc84a39ae11b8a3b36d46d
Guilherme <guilherme_h.10@hotmail.com>
2022-12-15 23:40:57 -0300

    Ligando a API + melhorias

--- cardio_app/lib/main.dart
+++ cardio_app/lib/main.dart
@@ -1,25 +1,18 @@
 import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter/widgets.dart';
+import 'dart:io' as Io;
 
 main() => runApp(const CardioApp());
 
 void teste() {
   // ignore: avoid_print
   print('TESTE');
-}
-
-void carregarArquivo() async {
-  FilePickerResult? result = await FilePicker.platform.pickFiles();
-
-  if (result != null) {
-    File file = File(result.files.single.path!);
-    //FALTA TRATAMENTO PRA VERIFICAR .CSVV
-
-  } else {
-    // User canceled the picker
-  }
-  return;
 }
 
 class CardioApp extends StatefulWidget {
@@ -30,8 +23,68 @@ class CardioApp extends StatefulWidget {
 }
 
 class _CardioAppState extends State<CardioApp> {
+  File? file;
+  var id;
   @override
   Widget build(BuildContext context) {
+    void apiTrain() async {
+      Response response;
+      var dio = Dio();
+      response = await dio.post(
+          'http://192.168.0.107:5000/api/v1/classification/train',
+          queryParameters: {
+            'files': await file!.readAsString(),
+            'target': 'DEATH_EVENT',
+          });
+      id = response.toString();
+    }
+
+    void carregarArquivoTreinamento() async {
+      FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+      if (result != null) {
+        setState(() {
+          file = File(result.files.single.path!);
+          apiTrain();
+        });
+        //FALTA TRATAMENTO PRA VERIFICAR .CSVV
+
+      } else {
+        // User canceled the picker
+      }
+    }
+
+    void apiPredicao() async {
+      Response response;
+      var dio = Dio();
+      response = await dio.post(
+          'http://192.168.0.107:5000/api/v1/classification/{$id}/predict',
+          queryParameters: {
+            'files': await file!.readAsString(),
+            'paraments': 'DEATH_EVENT',
+          });
+      var imgBase64 = response.toString();
+
+      final splitted = imgBase64.substring(47, 25628);
+      final decodedBytes = base64Decode(splitted);
+      var file = Io.File("decodedBezkoder.png");
+      file.writeAsBytesSync(decodedBytes);
+    }
+
+    void carregarArquivoPredicao() async {
+      FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+      if (result != null) {
+        setState(() {
+          file = File(result.files.single.path!);
+        });
+        //FALTA TRATAMENTO PRA VERIFICAR .CSVV
+
+      } else {
+        // User canceled the picker
+      }
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -42,14 +95,27 @@ class _CardioAppState extends State<CardioApp> {
           child: Column(
             // ignore: prefer_const_literals_to_create_immutables
             children: [
-              const Text(''),
-              const Card(
+              const Text('\nTreinamento'),
+              Card(
                 child: ListTile(
-                  title: Text('Carregar arquivo:'),
-                  subtitle: Text('Formato deve ser .CSV'),
+                  title: const Text('Carregar arquivo:'),
+                  subtitle:
+                      const Text('Formato deve ser .CSV para treino de modelo'),
                   trailing: IconButton(
-                      onPressed: carregarArquivo, icon: Icon(Icons.add)),
-                  iconColor: Color.fromARGB(255, 126, 0, 0),
+                      onPressed: carregarArquivoTreinamento,
+                      icon: const Icon(Icons.add)),
+                  iconColor: const Color.fromARGB(255, 126, 0, 0),
+                ),
+              ),
+              const Text('\nPredição'),
+              Card(
+                child: ListTile(
+                  title: const Text('Carregar arquivo:'),
+                  subtitle: const Text(
+                      'Formato deve ser .CSV para predição de valores'),
+                  trailing: IconButton(
+                      onPressed: apiPredicao, icon: const Icon(Icons.add)),
+                  iconColor: const Color.fromARGB(255, 126, 0, 0),
                 ),
               ),
             ],
--- servidor/main.py
+++ servidor/main.py
@@ -188,5 +188,6 @@ def predict_classifier_csv(model_id):
 
 
 if (__name__ == "__main__"):
-     app.run(port = 5000)
+     app.run(host='0.0.0.0', port=5000)
+
 
